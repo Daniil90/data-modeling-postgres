f7040b5064bef3dbb858ac7980722fcc05142344
Daniil90 <34265366+Daniil90@users.noreply.github.com>
2019-12-04 21:48:30 +0100

    Added drop tables and insert values queries.

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -62,19 +62,30 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT into songplays (songplay_id, user_id, level, song_id, 
+        artist_id, session_id, location, user_agent)
+        VALUES(s%, s%, s%, s%, s%, s%, s%, s%, s%)
 """)
 
 user_table_insert = ("""
+    INSERT into users (user_id, first_name, last_name, gender, level)
+    VALUES(s%, s%, s%, s%, s%)
 """)
 
 song_table_insert = ("""
+    INSERT into songs (song_id, title, artist_id, year, duration)
+    VALUES(s%, s%, s%, s%, s%)
 """)
 
 artist_table_insert = ("""
+    INSERT into artists (artist_id, name, location, latitude, longitude)
+    VALUES(s%, s%, s%, s%, s%)
 """)
 
 
 time_table_insert = ("""
+    INSERT into time (start_time, hour, day, week, month, year, weekday)
+    VALUES(s%, s%, s%, s%, s%, s%, s%)
 """)
 
 # FIND SONGS
